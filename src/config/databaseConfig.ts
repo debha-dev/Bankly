abd47e53c1be5fb7eab7b15bc33566a6d07591cc
debha-dev <web3dojo5@gmail.com>
2025-05-15 16:35:23 +0100

    alll

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -20,11 +20,11 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const sequelize = new Sequelize(databaseUrl,{
   dialect: "postgres",
-  host: process.env.DB_HOST, 
-  port: Number(process.env.DB_PORT) || 5432, 
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USER,    
-  password: process.env.DB_PASSWORD,  
+  // host: process.env.DB_HOST, 
+  // port: Number(process.env.DB_PORT) || 5432, 
+  // database: process.env.DB_DATABASE,
+  // username: process.env.DB_USER,    
+  // password: process.env.DB_PASSWORD,  
   
   dialectOptions: isProduction && process.env.DB_CA_CERTIFICATE ? {
     ssl: {
